4773727115a5b43d5df6747ea9e689fc139cc777
Kevin <kevdaviman.23@gmail.com>
2023-03-29 19:47:52 -0500

    add exercise 3

---
+++ Ejercicio3.js
@@ -0,0 +1,32 @@
+// Dada una palabra, buscarla en la frase y devolver cuantas 
+//veces aparece en la frase, la palabra debe ser un parametro
+// en la función
+
+function similarity(frase, search){
+    let cleanText = frase.toLowerCase().replace(/[!¡-.,]/gi, "");
+    let result = 0;
+
+    if(cleanText.includes(search)){
+        let words = cleanText.split(" ")
+        let map = {}
+
+        for(let word of words){
+            if(map[word]){
+                map[word]++
+
+            }else{
+                map[word] = 1
+            }
+        }
+
+        result = map[search]
+
+    }else{
+        result = 0
+    }
+    return result
+}
+console.log(
+    "Number of similarities in the frase: ",
+    similarity("hello, my Name is Kevin. You know, Kevin, like Kevin Arnold")
+);
